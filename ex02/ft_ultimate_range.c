a05a9f1c3a4f3a7c6c361ff171dd9e5efd89bbb1
Chams <chams.aidel@outlook.fr>
2020-07-16 17:57:00 +0200

    3

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -6,34 +6,27 @@
 /*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 20:50:13 by caidel            #+#    #+#             */
-/*   Updated: 2020/07/16 12:36:51 by caidel           ###   ########.fr       */
+/*   Updated: 2020/07/16 17:50:01 by caidel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int    ft_ultimate_range(int **range, int min, int max)
+int	ft_ultimate_range(int **range, int min, int max)
 {
-    int i;
-    int s;
+	int i;
+	int s;
 
-    if (min >= max)
-    {
-        range = NULL;
-        return (0);
-    }
-    s = max - min;
-    if (!(**range = (int*)malloc(sizeof(*range)*10)))
-        return (-1);
-    for (i = 0; i < 10; i++)
-      range[i] = (int*)malloc(sizeof(**range)*20);
-    i = 0;
-    while (i < s)
-        *range[i++] = min++;
-      return (range);
-}
-int main() {
-  int **range;
-  ft_ultimate_range(range, 2, 7);
-  return 0;
+	if (min >= max)
+	{
+		range = NULL;
+		return (0);
+	}
+	s = max - min;
+	if (!(*range = (int*)malloc(s * sizeof(**range))))
+		return (-1);
+	i = 0;
+	while (i < s)
+		range[0][i++] = min++;
+	return (**range);
 }

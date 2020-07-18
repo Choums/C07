c8c120eb108a4484757bc59bf91d9d470dc8179c
Chams <chams.aidel@outlook.fr>
2020-07-18 15:48:35 +0200

    ex3

---
+++ ex03/ft_strjoin.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 17:51:06 by caidel            #+#    #+#             */
+/*   Updated: 2020/07/18 15:46:51 by caidel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	j = 0;
+	i = 0;
+	while (dest[j])
+		j++;
+	while (src[i])
+	{
+		dest[i + j] = src[i];
+		i++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*scat;
+	int		i;
+	int		t;
+
+	t = 0;
+	i = 0;
+	while (i < size)
+	{
+		t += ft_strlen(strs[i++]);
+	}
+	i -= 1;
+	t += ft_strlen(sep) * i;
+	if (!(scat = (char*)malloc(t + sizeof(*scat))))
+		return (0);
+	scat[0] = '\0';
+	i = 0;
+	while (i < size)
+	{
+		ft_strcat(scat, strs[i++]);
+		if (i < size)
+			ft_strcat(scat, sep);
+	}
+	return (scat);
+}
